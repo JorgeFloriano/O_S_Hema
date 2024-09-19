fb97a7ea00e4dccef1cb7112b40456bfcebb465f
Jorge Luis <jorgefloriano1646@gmail.com>
2024-09-19 20:39:25 -0300

    PDF Creating

--- public/assets/js/pdf.js
+++ public/assets/js/pdf.js
@@ -1,16 +1,16 @@
 window.onload = function(){
     document.getElementById("btnPdf")
     .addEventListener("click", ()=>{
-        const relatorio = this.document.getElementById("relatorio");
+        const order = this.document.getElementById("print");
         const name = this.document.getElementById("header3").innerText;
-        console.log(relatorio);
+        console.log(order);
         var opt = {
             margin:        0,
             filename:     name+'.pdf',
             image:        { type: 'jpeg'},
-            html2canvas: { scale: 4, y: 0,  scrollY: 0},
+            html2canvas: { scale: 3, y: 0,  scrollY: 0},
             jsPDF:        { format: 'A4', orientation: 'portrait' }
           };
-        html2pdf().from(relatorio).set(opt).save();
+        html2pdf().from(order).set(opt).save();
     })
 }
---
+++ resources/views/o_s_parts/header.blade.php
@@ -0,0 +1,20 @@
+<div id="header">
+    <div id="header1">
+        <img src="{{ asset('assets/img/logo2_hema.png')}}" width="100%" alt="logo hema">
+    </div>
+
+    <div id="header2">
+        MONITORAMENTO IP<br>
+        CABEAMENTO ESTRUTURADO<br>
+        SINALIZAÇÃO SEMAFÓRICA<br>
+        <strong>
+            Fone: (15) 3243-4707<br>
+            <span>e-mail: atendimento@hema.com.br</span>
+        </strong>
+    </div>
+
+    <div id="header3">
+        SAT - Solicitação de Assistência Técnica
+        <div id="osId">{{$order->id}}</div>
+    </div>
+</div>
