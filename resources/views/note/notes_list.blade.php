a439c7fdf20513a5204f5e57fafc66de9a082311
Jorge Luis <jorgefloriano1646@gmail.com>
2025-01-28 20:31:23 -0300

    Problem description show/hidden

--- resources/views/note/notes_list.blade.php
+++ resources/views/note/notes_list.blade.php
@@ -30,12 +30,12 @@
                         Nenhum registro encontrado !
                     </p>
                 @else
-                    <table class="table table-striped" id="notes_list">
+                    <table class="table table-striped table-hover" id="notes_list">
                         <thead class="table-dark">
                             <tr>
                                 <th>O.S.</th>
                                 <th>Cliente</th>
-                                <th>Relato</th>
+                                <th>Problema relatado</th>
                                 <th>Data</th>
                                 <th>Exec.</th>
                                 <th>Encerr.</th>
@@ -47,7 +47,7 @@
                                 <tr>
                                     <td>{{number_format($order->id, 0, ',', '.')}}</td>
                                     <td>{{$order->client->name}}</td>
-                                    <td>{{mb_substr($order->req_descr, 0, 120, 'UTF-8')}}</td>
+                                    <td>{{$order->req_descr}}</td>
                                     <td>{{date('d/m/y',strtotime($order->req_date))}}</td>
 
                                     @if ($order->finished)
