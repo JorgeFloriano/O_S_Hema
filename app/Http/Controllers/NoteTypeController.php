797021518a74afc0c5cee8fc56418c6dbbdca03a
Jorge Luis <jorgefloriano1646@gmail.com>
2025-02-01 21:21:49 -0300

    Navbar updated

--- app/Http/Controllers/NoteTypeController.php
+++ app/Http/Controllers/NoteTypeController.php
@@ -39,7 +39,7 @@ class NoteTypeController extends Controller
             $opt = 1;
             $msg = 'Desativados';
             $cond = 'Ativar';
-            $title = 'Mostrar Ativos';
+            $title = 'Ativos';
             $btn_color = 'btn-success';
             $route = 'note_types.restore';
         } else {
@@ -47,7 +47,7 @@ class NoteTypeController extends Controller
             $opt = 0;
             $msg = 'Ativos';
             $cond = 'Desativar';
-            $title = 'Mostrar Desativados';
+            $title = 'Desativados';
             $btn_color = 'btn-danger';
             $route = 'note_types.desativate';
         }
