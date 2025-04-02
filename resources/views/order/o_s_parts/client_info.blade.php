4e909aa9daf985d5e3ccf5e03a122b5cfd7d57de
Jorge Luis <jorgefloriano1646@gmail.com>
2025-04-01 21:38:45 -0300

    updated csv all data

--- resources/views/order/o_s_parts/client_info.blade.php
+++ resources/views/order/o_s_parts/client_info.blade.php
@@ -25,7 +25,7 @@
     </div>
 
     <div class="InfoLine">
-        <div class="InfoFirstCollum" style="width: 50%"><strong>Tipo de Serviço: </strong>CFTV</div>
+        <div class="InfoFirstCollum" style="width: 50%"><strong>Tipo de Serviço: </strong>{{$order->type->description}}</div>
         <div class="InfoCollum"><strong>Equipamento : </strong>{{$order->equipment ?? ''}}</div>
     </div>
 
