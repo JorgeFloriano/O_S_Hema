c888429d5ea2896f52aa5627a621b7afa7261579
Jorge Luis <jorgefloriano1646@gmail.com>
2026-02-14 23:07:38 -0300

    User Permissions implement

--- resources/views/user/user_create.blade.php
+++ resources/views/user/user_create.blade.php
@@ -56,54 +56,121 @@
                             <label for="password_confirmation">Confirmar Senha</label>
                         </div>
 
-                        <fieldset><legend>Selecione um ou mais perfis:</legend><br/>
-                            <div id="hema_profiles">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="1" name="tec" id="tec">
-                                    <label class="form-check-label" for="tec">
-                                        <strong>Técnico (Hema)</strong>
-                                    </label>
-                                </div>
-                                <div class="form-check">
-                                    <input onchange="enableDisable(['cli'])" class="form-check-input" type="checkbox" value="1" name="adm" id="adm">
-                                    <label class="form-check-label" for="adm">
-                                        <strong>Administrador (Hema)</strong>
-                                    </label>
-                                
-                                    <div class="form-check">
-                                        <input disabled class="form-check-input" type="checkbox" value="1" name="cli" id="cli">
-                                        <label class="form-check-label" for="cli">
-                                            Acesso a Clientes e Materiais
-                                        </label>
+                        
+                        <div class="my-4">
+                            <h5 class="form-label">Tipo de Cadastro:</h5>
+
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input"
+                                 type="radio" name="type_user" id="hema_user" value="1">
+                                <label class="form-check-label" for="hema_user">Usuário Hema</label>
+                            </div>
+                              
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input"
+                                 type="radio" name="type_user" id="client_user" value="2">
+                                <label class="form-check-label" for="client_user">Usuário Cliente</label>
+                            </div>
+                        </div>
+
+                        <div class="mt-4" id="hema_field">
+                            <h5 class="form-label">Selecionar permissões:</h5>
+                            
+                            <div class="row g-0 mb-3">
+                                <div class="col-md-4">
+                                    <button class="btn btn-outline-primary w-100 my-1 first-group-btn collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAdm">
+                                        Administrador 
+                                    </button>
+                                    <div class="collapse multi-collapse" id="collapseAdm">
+                                        <div class="card card-body shadow-sm mx-1 my-2">
+                                            <h6>Módulos Administrativos</h6>
+                                            <hr>
+                                            
+                                            @php
+                                                $modulos = [
+                                                    'sats' => 'SATs',
+                                                    'materials' => 'Materiais',
+                                                    'clients' => 'Clientes',
+                                                    'codes' => 'Códigos'
+                                                    ];
+                                                if (auth()->user()->isMainAdm()) {
+                                                    $modulos['users'] = 'Usuários';
+                                                }
+                                            @endphp
+                                            
+                                            @foreach($modulos as $key => $label)
+                                                <div class="mb-2 d-flex justify-content-between align-items-center">
+                                                    <label class="small fw-bold mb-0 text-nowrap" for="permission_{{ $key }}">{{ $label }}</label>
+                                                    <select name="permissions[{{ $key }}]" class="form-select form-select-sm ms-2" style="max-width: 160px;" id="permission_{{ $key }}">
+                                                        <option value="0">Sem Acesso</option>
+                                                        <option value="1">Somente Leitura</option>
+                                                        <option value="2">Acesso Completo</option>
+                                                    </select>
+                                                </div>
+                                            @endforeach
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="1" name="sup" id="sup">
-                                    <label class="form-check-label" for="sup">
-                                        <strong>Supervisor (Hema)</strong>
-                                    </label>
+                                <div class="col-md-4">
+                                    <button class="btn btn-outline-primary w-100 my-1 mid-group-btn collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSup">
+                                        Supervisor
+                                    </button>
+                                    <div class="collapse multi-collapse" id="collapseSup">
+                                        <div class="card card-body shadow-sm mx-1 my-2">
+                                            <h6>Ações de Supervisão</h6>
+                                            <hr>
+
+                                            <div class="form-check mb-2">
+                                                <input class="form-check-input" type="checkbox" id="compl_sup_access" name="permissions[compl_sup_access]" value="2">
+                                                <label class="form-check-label" for="compl_sup_access">Acesso Completo</label>
+                                            </div>
+
+                                            <div class="form-check form-switch mb-2">
+                                                <input class="form-check-input sup-acess" type="checkbox" id="attach_tec" name="permissions[attach_tec]" value="2">
+                                                <label class="form-check-label" for="attach_tec">Vincular SAT / Técnico</label>
+                                            </div>
+
+                                            <div class="form-check form-switch mb-2">
+                                                <input class="form-check-input sup-acess" type="checkbox" id="manager_on_call" name="permissions[manager_on_call]" value="2">
+                                                <label class="form-check-label" for="manager_on_call">Gerenciar Sobreaviso</label>
+                                            </div>
+
+                                            <div class="form-check form-switch mb-2">
+                                                <input class="form-check-input sup-acess" type="checkbox" id="reopen_sat" 
+                                                    name="permissions[reopen_sat]" value="2">
+                                                <label class="form-check-label" for="reopen_sat">Reabrir SAT</label>
+                                            </div>
+
+                                            <small class="text-muted">* Visualizar SATs é padrão para supervisores.</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <button class="btn btn-outline-primary w-100 my-1 last-group-btn collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTec">
+                                        Técnico 
+                                    </button>
+                                    <div class="collapse multi-collapse" id="collapseTec">
+                                        <div class="card card-body shadow-sm mx-1 my-2">
+                                            <h6>Perfil Técnico</h6>
+                                            <hr>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" id="tech_access" 
+                                                    name="permissions[tech_access]" value="2">
+                                                <label class="form-check-label" for="tech_access">Habilitar Acesso</label>
+                                            </div>
+                                            <p class="small text-muted mt-2">O técnico visualiza apenas sua própria programação de serviços.</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
 
-                            <div class="form-check">
-                                <input onchange="
-                                    enableDisable(['tec', 'adm', 'sup']),
-                                    showAndHideElement('client_select', 'hema_profiles')"
-                                    class="form-check-input"
-                                    type="checkbox"
-                                    value="1"
-                                    name="user_client"
-                                    id="user_client">
-                                <label class="form-check-label" for="user_client">
-                                    <strong>Cliente (Clientes)</strong>
-                                </label>
+                        <div id='client_field'>
+                            <div id="client_select">
+                                <x-datalist :objs="$clients" obj="client" tit="Selecionar Cliente" :val="old('client')" des="name"/>
                             </div>
-
-                            <div id="client_select" style="display: none;">
-                                <x-datalist :objs="$clients" obj="client" tit="Cliente" :val="old('client')" des="name"/>
-                            </div>
-                        </fieldset>
-
+                        </div>
+                        
                         <div class="my-2">
                             <button id="submitButton" type="submit" class="btn btn-primary me-2" data-bs-dismiss="modal">
                                 <i class="fa fa-check"></i> Confirma
@@ -118,3 +185,17 @@
         </div>
     </div>
 @endsection
+
+<script>
+    // Executa após o carregamento do DOM
+    document.addEventListener('DOMContentLoaded', function() {
+        // Ativa a lógica para o grupo de Supervisor
+        setupPermissionGroup('compl_sup_access', 'sup-acess');
+        
+        // Se no futuro tiver um grupo de Admin, basta adicionar uma linha:
+        // setupPermissionGroup('compl_adm_access', 'adm-acess');
+
+        // Nova lógica de alternância de tipo de usuário
+        setupUserTypeToggle();
+    });
+</script>
